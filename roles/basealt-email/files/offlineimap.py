3dc7bb3b746bccf07d386665927d37ac072f17d7
Alexey Sheplyakov <asheplyakov@basealt.ru>
2022-08-24 00:28:34 +0400

    basealt-email: support python2 based offlineimap

--- roles/basealt-email/files/offlineimap.py
+++ roles/basealt-email/files/offlineimap.py
@@ -1,5 +1,12 @@
 
+import sys
 from subprocess import check_output
 
+
 def get_pass(key):
-    return check_output(['pass', key], encoding='utf-8').splitlines()[0]
+    if sys.version_info.major == 3:
+        return check_output(['pass', key], encoding='utf-8').splitlines()[0]
+    elif sys.version_info.major == 2:
+        return check_output(['pass', key]).splitlines()[0]
+    else:
+        raise RuntimeError("unsupported python version")
